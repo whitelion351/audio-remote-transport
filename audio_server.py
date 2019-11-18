814dfedc867a6044f3ed4c1add64c24ee935e193
whitelion351 <56313776+whitelion351@users.noreply.github.com>
2019-11-18 00:10:48 -0700

    many updates and fixes
    biggest fix was using a time.sleep statement instead of pass in the buffer code to reduce CPU usage by a lot.

--- audio_server.py
+++ audio_server.py
@@ -1,16 +1,16 @@
-import pyaudio
-import audioread
 import socket
 from threading import Thread
 import argparse
 import time
 import numpy as np
+import pyaudio
+import audioread
 
 
 class AudioServer:
     # noinspection SpellCheckingInspection
     def __init__(self, filename=None, chunk=2048, audio_format=pyaudio.paInt16, channels=1, rate=44100,
-                 bind_address="0.0.0.0", bind_port=1060, audio_buffer_size=96, buffer_size_increment=6,
+                 bind_address="0.0.0.0", bind_port=1060, audio_buffer_size=102, buffer_size_increment=6,
                  buffer_optimize_time=10, use_compression=0, config_filename="AudioServer_devices.cfg",
                  configure_devices=False, input_device_index=None, output_device_index=None):
         # constants
@@ -26,6 +26,8 @@ class AudioServer:
         self.output_device_index = output_device_index
         self.need_to_configure = False
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind_address = bind_address
+        self.bind_port = bind_port
         self.connection.bind((bind_address, bind_port))
         self.connection.listen(5)
         self.clients = {}
@@ -39,7 +41,6 @@ class AudioServer:
         self.buffer_id = -1
         self.highest_buffer_pos = 1
         self.use_compression = use_compression
-        print("audio server running on {}:{}".format(bind_address, bind_port))
 
         # parse command line arguments
         parser = argparse.ArgumentParser(description="Server portion of audio transport")
@@ -106,22 +107,22 @@ class AudioServer:
     def configure_this_instance(self, instance):
         print("Listing available APIs")
         host_api_count = instance.get_host_api_count()
-        infos = []
+        info_list = []
         for i in range(host_api_count):
             info = instance.get_host_api_info_by_index(i)
             print(i+1, ") ", info, sep="")
-            infos.append(info)
-        if len(infos) < 1:
+            info_list.append(info)
+        if len(info_list) < 1:
             print("No APIs available. Configure can not continue. Hopefully the defaults work for you")
             return
         user_choice = input("Choose api: ")
         user_choice = int(user_choice) - 1
-        host_api_info = infos[user_choice]
+        host_api_info = info_list[user_choice]
         host_api_index = host_api_info["index"]
         print("using host api:", host_api_info["name"])
 
         print("Listing devices available to the API")
-        infos = []
+        info_list = []
         for i in range(instance.get_device_count()):
             info = instance.get_device_info_by_index(i)
             if info["hostApi"] == host_api_index:
@@ -129,18 +130,18 @@ class AudioServer:
                     i+1, info["name"], info["maxInputChannels"], info["maxOutputChannels"],
                     info["defaultSampleRate"], info["index"]
                 ))
-                infos.append(info)
-        if len(infos) < 1:
+                info_list.append(info)
+        if len(info_list) < 1:
             print("No devices available for this API. Configure can not continue. Hopefully the defaults work for you")
             return
         user_choice = input("Choose input device: ")
         user_choice = int(user_choice) - 1
-        input_device_info = infos[user_choice]
+        input_device_info = info_list[user_choice]
         input_device_index = input_device_info["index"]
         print("using {} for input".format(input_device_info["name"]))
         user_choice = input("Choose output device: ")
         user_choice = int(user_choice) - 1
-        output_device_info = infos[user_choice]
+        output_device_info = info_list[user_choice]
         output_device_index = output_device_info["index"]
         print("using {} for output".format(input_device_info["name"]))
         self.input_device_index = input_device_index
@@ -154,34 +155,42 @@ class AudioServer:
         if "rolling_buffer" not in self.threads.keys():
             print("starting server buffer thread")
             self.begin_rolling_buffer()
-        clientsocket, address = self.connection.accept()
-        clientsocket.settimeout(5)
-        try:
-            msg = clientsocket.recv(self.CHUNK)
-            decodedmsg = msg.decode("utf-8").split(sep=",")
-            print("connection received from {}".format(address))
-            if decodedmsg[0] == "AudioClient" and len(decodedmsg) > 1:
-                print("type is {} with buffer size {}. sending audio parameters".format(decodedmsg[0], decodedmsg[1]))
-                params = "{},{},{}".format(self.RATE, self.CHUNK, self.use_compression)
-                clientsocket.send(bytes(params, "utf-8"))
-                msg = clientsocket.recv(self.CHUNK)
-                if msg.decode("utf-8") == "ok":
-                    print("creating client thread {}".format(address[0]))
-                    self.clients[address[0]] = clientsocket
-                    thread = Thread(target=self.send_audio_loop, name=address[0],
-                                    daemon=True, args=(clientsocket, address[0], int(decodedmsg[1])))
-                    self.threads[thread.name] = thread
-                    thread.start()
+        print("audio server running on {}:{}".format(self.bind_address, self.bind_port))
+        while True:
+            client_socket, address = self.connection.accept()
+            client_socket.settimeout(5)
+            try:
+                msg = client_socket.recv(self.CHUNK)
+                decoded_msg = msg.decode("utf-8").split(sep=",")
+                print("connection received from {}".format(address))
+                if decoded_msg[0] == "AudioClient" and len(decoded_msg) > 1:
+                    print("type is {} with buffer size {}. sending audio parameters".format(decoded_msg[0], decoded_msg[1]))
+                    params = "{},{},{}".format(self.RATE, self.CHUNK, self.use_compression)
+                    client_socket.send(bytes(params, "utf-8"))
+                    msg = client_socket.recv(self.CHUNK)
+                    d_msg = ""
+                    try:
+                        d_msg = msg.decode("utf-8")
+                    except UnicodeDecodeError as e:
+                        print(e)
+                    if d_msg == "ok":
+                        print("creating client thread {}".format(address[0]))
+                        self.clients[address[0]] = client_socket
+                        thread = Thread(target=self.send_audio_loop, name=address[0],
+                                        daemon=True, args=(client_socket, address[0], int(decoded_msg[1])))
+                        self.threads[thread.name] = thread
+                        thread.start()
+                    else:
+                        d_msg = "'" + d_msg + "' "
+                        print("received {} {}after sending parameters. closing connection".format(msg, d_msg))
+                        client_socket.close()
                 else:
-                    print("invalid response received after sending audio parameters. closing connection")
-                    clientsocket.close()
-            else:
-                print("invalid identity {}. terminating connection".format(decodedmsg))
-                clientsocket.send(bytes("i have nothing for you", "utf-8"))
-                clientsocket.close()
-        except ConnectionError as e:
-            print("ConnectionError:", e.errno, e.strerror)
-            clientsocket.close()
+                    print("invalid identity {}. terminating connection".format(decoded_msg))
+                    client_socket.send(bytes("i have nothing for you", "utf-8"))
+                    client_socket.close()
+            except ConnectionError as e:
+                print("ConnectionError:", e.errno, e.strerror)
+                client_socket.close()
 
     def begin_rolling_buffer(self):
         thread = Thread(target=self.rolling_buffer, name="rolling buffer", daemon=True)
@@ -212,7 +221,7 @@ class AudioServer:
                     self.buffer_size -= self.buffer_size_increment
                     print("max load {} / {}. dropping size to {}".format(self.highest_buffer_pos,
                                                                          len(self.audio_buffer), self.buffer_size))
-                else:
+                elif len(self.clients) > 0:
                     print("max load {} / {}".format(self.highest_buffer_pos, len(self.audio_buffer)))
                 last_buffer_optimize = time.time()
                 self.highest_buffer_pos = 1
@@ -283,7 +292,7 @@ class AudioServer:
                     total_data_cords += 1
                     direction = 1 if data[cursor + 1] >= value else -1
             last_value = value
-        if total_data_cords != len(data_cords) or total_data_cords != len(new_data):
+        if len(data_cords) != len(new_data):
             print("compress error. tot cords {} len cords {} len data {}".format(total_data_cords, len(data_cords),
                                                                                  len(new_data)))
         new_data = [total_data_cords] + data_cords + new_data
@@ -293,13 +302,14 @@ class AudioServer:
     def send_audio_loop(self, client_socket, address, client_buffer_size):
         done = False
         current_buffer_id = self.buffer_id - client_buffer_size
-        cur_buf_pos = client_buffer_size
+        cur_buf_pos = client_buffer_size if client_buffer_size < len(self.audio_buffer) - self.buffer_size_increment \
+            else len(self.audio_buffer) - self.buffer_size_increment
+        print("client starting at buffer position", cur_buf_pos)
         while not done:
             try:
                 if cur_buf_pos < 1:
                     cur_buf_pos = 1
                     current_buffer_id = self.buffer_id
-                    print("server buffer is lagging")
                 elif cur_buf_pos > len(self.audio_buffer):
                     cur_buf_pos = len(self.audio_buffer)
                     current_buffer_id = self.buffer_id - len(self.audio_buffer)
@@ -332,6 +342,7 @@ class AudioServer:
                                                                 moved_positions))
                 # end buffer magic
 
+                header = None
                 if self.use_compression > 0:
                     header = len(next_chunk).to_bytes(2, "little", signed=True)
                     client_socket.send(header)
@@ -340,8 +351,7 @@ class AudioServer:
                         raise ConnectionError("client responded to data size {} with {}".format(header, msg))
                 client_socket.send(next_chunk)
                 msg = client_socket.recv(self.CHUNK)
-                if current_buffer_id + 1 <= self.buffer_id:
-                    current_buffer_id += 1
+                current_buffer_id += 1
                 cur_buf_pos = (self.buffer_id - current_buffer_id) + 1
                 if cur_buf_pos > self.highest_buffer_pos:
                     self.highest_buffer_pos = cur_buf_pos if cur_buf_pos <= self.buffer_size else self.buffer_size
@@ -354,20 +364,20 @@ class AudioServer:
                 self.close_connection(client_socket, address)
             else:
                 try:
-                    decodedmsg = msg.decode("utf-8")
+                    decoded_msg = msg.decode("utf-8")
                 except UnicodeDecodeError:
                     print("error decoding client msg after sent data. client sent {} instead of ok".format(msg))
                     if self.use_compression > 0 and msg == header:
                         print("that response matches the sent header")
                 else:
-                    if decodedmsg != "ok":
-                        print("client said '{}' so ending audio loop".format(decodedmsg))
+                    if decoded_msg != "ok":
+                        print("client said '{}' so ending audio loop".format(decoded_msg))
                         self.close_connection(client_socket, address)
                         done = True
 
-    def close_connection(self, clientsocket, address):
+    def close_connection(self, client_socket, address):
         try:
-            clientsocket.close()
+            client_socket.close()
             del self.clients[address]
             print("{} clients remaining".format(len(self.clients)))
         except KeyError:
@@ -386,7 +396,7 @@ class AudioServer:
     def get_next_chunk(self):
         if self.filename is None:
             while self.live_stream.get_read_available() < self.CHUNK:
-                pass
+                time.sleep(0.1)
             data = self.live_stream.read(self.CHUNK)
 #             print("{} frames left to read".format(self.live_stream.get_read_available()))
         else:
@@ -401,7 +411,6 @@ class AudioServer:
 
 
 if __name__ == "__main__":
-    audio = AudioServer(audio_buffer_size=120,  use_compression=0)
+    audio = AudioServer(rate=44100, audio_buffer_size=108,  configure_devices=False, use_compression=0)
     # audio = AudioServer(filename="freq_test.opus")
-    while True:
-        audio.wait_for_connection()
+    audio.wait_for_connection()
